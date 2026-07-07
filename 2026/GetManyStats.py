af7b4bdb0386d249ebe6beb3ff611850b5fa5917
d1s0b3y <disob1d2@gmail.com>
2026-07-07 13:48:45 -0400

    made work since sb is back up

--- 2026/GetManyStats.py
+++ 2026/GetManyStats.py
@@ -8,24 +8,27 @@ teams=sb.get_teams(metric='norm_epa',limit=teamAmt,fields=['team','name'])
 print("finding teams")
 
 for team in teams:
-    teamStats=sb.get_team_year(team['team'],2026)
+    # teamStats=sb.get_team_year(team['team'],2026)
+    teamStats=sb.get_team_year(team,2026)
     total_points=teamStats['epa']
     points = total_points['total_points']
-    mean=points['mean']
+    mean=points
     breakdown=total_points['breakdown']
     record=teamStats['record']
     wins=record['wins']
     losses=record['losses']
-    if teamStats['competing']['next_event_key']=='2026cmptx':
-        qualified=True
-    else:
-        qualified=False
-    statsLst.append({"Team number":team['team'],"Team name":team['name'],"Epa":mean,"Auto EPA":breakdown['auto_points'],"Teleop EPA":breakdown['teleop_points'],"Energized":breakdown['energized_rp'], "Supercharged":breakdown['supercharged_rp'],"Transition":breakdown['transition_fuel'],"First Shift":breakdown['first_shift_fuel'],"Second Shift":breakdown['second_shift_fuel'],"Endgame":breakdown['endgame_fuel'],"Win Rate":teamStats['record']['winrate'],"Wins":wins,"Losses":losses})
-    print(f"gathered stats for {team['name']}      {teams.index(team)+1} teams queried, {len(teams)-teams.index(team)-1} remaining",)
+    # if teamStats['competing']['next_event_key']=='2026cmptx':
+    #     qualified=True
+    # else:
+    #     qualified=False
+    statsLst.append({"Team number":team,"Team name":teamStats['name'],"Epa":mean,"Auto EPA":breakdown['auto_points'],"Teleop EPA":breakdown['teleop_points'],"Energized":breakdown['energized_rp'], "Supercharged":breakdown['supercharged_rp'],"Transition":breakdown['transition_fuel'],"First Shift":breakdown['first_shift_fuel'],"Second Shift":breakdown['second_shift_fuel'],"Endgame":breakdown['endgame_fuel'],"Win Rate":teamStats['record']['winrate'],"Wins":wins,"Losses":losses})
+    # statsLst.append({"Team number":team['team'],"Team name":team['name'],"Epa":mean,"Auto EPA":breakdown['auto_points'],"Teleop EPA":breakdown['teleop_points'],"Energized":breakdown['energized_rp'], "Supercharged":breakdown['supercharged_rp'],"Transition":breakdown['transition_fuel'],"First Shift":breakdown['first_shift_fuel'],"Second Shift":breakdown['second_shift_fuel'],"Endgame":breakdown['endgame_fuel'],"Win Rate":teamStats['record']['winrate'],"Wins":wins,"Losses":losses})
+    print(f"gathered stats for {team}      {teams.index(team)+1} teams queried, {len(teams)-teams.index(team)-1} remaining",)
+    # print(f"gathered stats for {team['name']}      {teams.index(team)+1} teams queried, {len(teams)-teams.index(team)-1} remaining",)
 
 fieldnames=statsLst[0].keys()
 
-with open('500teams.csv', 'a', newline='',encoding='utf-8') as file:
+with open('.csv', 'a', newline='',encoding='utf-8') as file:
     writer = csv.DictWriter(file,fieldnames=fieldnames)
     writer.writeheader()
     writer.writerows(statsLst)
--- 2026/TBAstats.py
+++ 2026/TBAstats.py
@@ -1,4 +1,7 @@
-# DO NOT SHARE!!!!!!            API_KEY = StFmcbQSa2R3CgVBAyKOVxwxEprPStQxseGkMcZqVjghT421r6OIVYPbvxCcxSky 
+# DO NOT SHARE!!!!!!                                                                                                                                                                                                                                                                                                                                                                                 API_KEY = StFmcbQSa2R3CgVBAyKOVxwxEprPStQxseGkMcZqVjghT421r6OIVYPbvxCcxSky 
+
+TBA_API_KEY="StFmcbQSa2R3CgVBAyKOVxwxEprPStQxseGkMcZqVjghT421r6OIVYPbvxCcxSky"
+
 import requests
 import json
 def ping(url:str,headConditions:dict={},name=None):
@@ -22,7 +25,7 @@ def get_team_events_data(team: int):
     events={}
     # getting event codes
     print("getting event codes")
-    TBAresponse=requests.get(f"https://www.thebluealliance.com/api/v3/team/{'frc'+str(team)}/events/2026/keys",headers={"X-TBA-Auth-Key":"StFmcbQSa2R3CgVBAyKOVxwxEprPStQxseGkMcZqVjghT421r6OIVYPbvxCcxSky"})
+    TBAresponse=requests.get(f"https://www.thebluealliance.com/api/v3/team/{'frc'+str(team)}/events/2026/keys",headers={"X-TBA-Auth-Key":TBA_API_KEY})
     TBAtext=TBAresponse.text.split('"')
 
     # formating event codes
@@ -45,7 +48,7 @@ def get_team_events_data(team: int):
 team=3414
 # SBparams={"metric":"norm_epa","limit":2}
 
-ping(url="https://www.thebluealliance.com/api/v3/status",name="TBA",headConditions={"headers":{"X-TBA-Auth-Key":"StFmcbQSa2R3CgVBAyKOVxwxEprPStQxseGkMcZqVjghT421r6OIVYPbvxCcxSky"}})
+ping(url="https://www.thebluealliance.com/api/v3/status",name="TBA",headConditions={"headers":{"X-TBA-Auth-Key":TBA_API_KEY}})
 ping(url="https://api.statbotics.io/openapi.json",name="SB")
 
 # response=get_team_events_data(team=team)
